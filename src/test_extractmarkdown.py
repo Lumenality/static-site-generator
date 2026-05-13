4b8c6c50bde39dc1748754241cc7ac47f0e75616
Lumenality <sebastian.t.skoglund@gmail.com>
2026-05-13 02:10:42 +0200

    Wrote one test for the markdown regex link parser

--- src/test_extractmarkdown.py
+++ src/test_extractmarkdown.py
@@ -1,11 +1,12 @@
 import unittest
 from extractmarkdown import extract_markdown_images,extract_markdown_links
 
-def test_extract_markdown_images(self):
-    matches = extract_markdown_images(
-        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
-    )
-    self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
-    
+class TestExtractMarkdown(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+ 
 if __name__ == "__main__":
     unittest.main()
